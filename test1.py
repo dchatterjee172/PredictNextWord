dfdd8e95f069fe2c9057c621044a5d53c4346d2a
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2017-10-08 00:48:42 +0530

    update

--- test1.py
+++ test1.py
@@ -8,9 +8,9 @@ good_sentence=list()
 bad_sentence=list()
 total_sen=list()
 wordvec=0
-worddimy=10
-worddimx=10
-hiddens=6
+worddimy=25
+worddimx=20
+hiddens=100
 _loop=1
 def sig(signal,frame):
 	global _loop
@@ -25,6 +25,7 @@ def WordtoVec():
 	dt=tf.float32
 	pre=tf.placeholder(name="pre",dtype=dt,shape=(1,hiddens))
 	coef=tf.placeholder(name="coef",dtype=dt,shape=(worddimx,worddimy,hiddens))
+	coef1=tf.placeholder(name="coef",dtype=dt,shape=(hiddens,1))
 	coef2=tf.placeholder(name="coef",dtype=dt,shape=(1,hiddens))
 	actual=tf.placeholder(name="actual",dtype=dt,shape=(1,1))
 	pregrad=tf.placeholder(name="stategradactual",dtype=dt,shape=(1,hiddens))
@@ -35,23 +36,25 @@ def WordtoVec():
 	v=tf.constant(0)
 	calrawstate=tf.while_loop(cond,body,[rawstate,x,v,coef])[0]
 	state=(calrawstate+tf.multiply(pre,coef2))
-	final=tf.nn.softsign(tf.matmul(state,tf.ones(shape=(hiddens,1))))
+	final=tf.nn.softsign(tf.matmul(state,coef1))
 	loss=tf.square(final-actual)
 	dxlast=tf.gradients(loss,x,name="lastxgrad")
 	dstatelast=tf.gradients(loss,state,name="laststategrad")
 	dcoeflast=tf.gradients(loss,coef,name="laststategrad")
 	dpre=tf.gradients(state,pre,grad_ys=pregrad,name="pregrad")
 	dx=tf.gradients(state,x,grad_ys=pregrad,name="xgrad")
-	dcoef=tf.gradients(state,coef,grad_ys=pregrad,name="xgrad")
+	dcoef1=tf.gradients(loss,coef1,name="dcoef1")
 	_graph=open("~graph","w")
 	with tf.Session() as sess:
 		writer = tf.summary.FileWriter("tfg", sess.graph)
 		co=np.random.uniform(-2,2,(worddimx,worddimy,hiddens))
+		co1=np.random.uniform(1,1,(hiddens,1))
 		co2=np.random.uniform(0,1,(1,hiddens))
+		dco1=np.zeros(shape=(hiddens,1))
 		_i=0
 		_bloss=0
 		_loss=0
-		batch_mem=20
+		batch_mem=10
 		trainingpbatch=0
 		it=0
 		c=0
@@ -73,6 +76,8 @@ def WordtoVec():
 					print(it," loss ",_bloss/batch_mem)
 					c+=1
 					_graph.write(str(c)+" "+str(_bloss/batch_mem)+"\n")
+					#co1-=dco1*.01
+					#dco1=np.zeros(shape=(hiddens,1))
 				_bloss=0
 			states=np.zeros(shape=(len(words)-1,1,hiddens))
 			res=np.array(int(words[len(words)-1])).reshape(1,1)
@@ -88,7 +93,7 @@ def WordtoVec():
 					sess.run([assignx],feed_dict=inp)
 					states[w]=sess.run(state,feed_dict=inp)
 				else:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2}
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
 					z=sess.run([state,loss],feed_dict=inp)
 					#print(z[0][0],res)
 					states[w]=z[0]
@@ -97,17 +102,16 @@ def WordtoVec():
 			if(_loss<0.00000001):
 				continue
 			dpres=[[0]]
-			_dcoef=np.zeros(shape=(worddimx,worddimy,hiddens))
 			for w in reversed(range(0,len(words)-1)):
 				k=int(words[w])
 				x_=wordvec[k].reshape(worddimx,1,worddimy)
 				if w==len(words)-2:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2}
-					z=sess.run([dxlast,dstatelast],feed_dict=inp)
-					wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.01
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
+					z=sess.run([dxlast,dstatelast,dcoef1],feed_dict=inp)
+					wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.001*_loss
+					dco1+=z[2][0]
 					#print(z[0][0].reshape(worddimx,worddimy)*.5)
-					#_dcoef+=z[2][0]
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:z[1][0],coef2:co2}
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:z[1][0],coef2:co2,coef1:co1}
 					dpres=(sess.run(dpre,feed_dict=inp))[0]
 				else:
 					if w>0:
@@ -115,14 +119,13 @@ def WordtoVec():
 						z=sess.run([dpre,dx],feed_dict=inp)
 						dpres=z[0][0]
 						#_dcoef+=z[2][0]
-						wordvec[k]-=z[1][0].reshape(worddimx,worddimy)*.01
+						wordvec[k]-=z[1][0].reshape(worddimx,worddimy)*.001*_loss
 						#print(z[1][0].reshape(worddimx,worddimy)*.4)
 					else:
 						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,actual:res,pregrad:dpres,coef2:co2}
 						z=sess.run([dx],feed_dict=inp)
-						wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.01
+						wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.001*_loss
 						#print(z[0][0].reshape(worddimx,worddimy)*.4)
-						#_dcoef+=z[1][0]
 				#co-=_dcoef*.1
 	_graph.close()
 signal.signal(signal.SIGINT, sig)
