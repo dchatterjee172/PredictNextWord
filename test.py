d916e08b4a0ce45e819c020248b709d547c3ceb5
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2017-10-10 16:08:53 +0530

    updated the main file

--- test.py
+++ test.py
@@ -3,28 +3,29 @@ import numpy as np
 import data
 import data1
 import signal
-dictionary=set()
 good_sentence=list()
 bad_sentence=list()
 total_sen=list()
 wordvec=0
-worddimy=5
-worddimx=10
-hiddens=20
+worddimy=200
+worddimx=1
+hiddens=50
 _loop=1
 def sig(signal,frame):
 	global _loop
 	_loop=0
 def body(rawstate,x,v,coef):
-	rawstate+=tf.matmul(x[v],coef[v])
-	v=v+1
-	return [rawstate,x,v,coef]
+	_r=rawstate+tf.matmul(x[v],coef[v])
+	_v=v+1
+	return [_r,x,_v,coef]
 def cond(rawstate,x,v,coef):
 	return v<worddimx
 def WordtoVec():
 	dt=tf.float32
 	pre=tf.placeholder(name="pre",dtype=dt,shape=(1,hiddens))
 	coef=tf.placeholder(name="coef",dtype=dt,shape=(worddimx,worddimy,hiddens))
+	coef1=tf.placeholder(name="coef",dtype=dt,shape=(hiddens,1))
+	coef2=tf.placeholder(name="coef",dtype=dt,shape=(hiddens,hiddens))
 	actual=tf.placeholder(name="actual",dtype=dt,shape=(1,1))
 	pregrad=tf.placeholder(name="stategradactual",dtype=dt,shape=(1,hiddens))
 	x=tf.get_variable("x1",shape=[worddimx,1,worddimy])
@@ -32,25 +33,28 @@ def WordtoVec():
 	assignx=tf.assign(x,actx)
 	rawstate=tf.zeros(shape=(1,hiddens),dtype=dt,name="rawstate")
 	v=tf.constant(0)
-	calrawstate=tf.while_loop(cond,body,[rawstate,x,v,coef])
-	state=(calrawstate[0]+pre)
-	final=tf.nn.softsign(tf.matmul(state,tf.ones(shape=(hiddens,1))))
+	calrawstate=tf.while_loop(cond,body,[rawstate,x,v,coef])[0]
+	state=(calrawstate+tf.matmul(pre,coef2))
+	final=tf.nn.softsign(tf.matmul(state,coef1))
 	loss=tf.square(final-actual)
 	dxlast=tf.gradients(loss,x,name="lastxgrad")
 	dstatelast=tf.gradients(loss,state,name="laststategrad")
 	dcoeflast=tf.gradients(loss,coef,name="laststategrad")
 	dpre=tf.gradients(state,pre,grad_ys=pregrad,name="pregrad")
 	dx=tf.gradients(state,x,grad_ys=pregrad,name="xgrad")
-	dcoef=tf.gradients(state,coef,grad_ys=pregrad,name="xgrad")
+	dcoef1=tf.gradients(loss,coef1,name="dcoef1")
 	_graph=open("~graph","w")
 	with tf.Session() as sess:
 		writer = tf.summary.FileWriter("tfg", sess.graph)
-		co=np.random.uniform(-3,3,(worddimx,worddimy,hiddens))
+		co=np.random.uniform(-.5,.5,(worddimx,worddimy,hiddens))
+		co1=np.random.uniform(-.2,.2,(hiddens,1))
+		co2=np.random.uniform(-.15,.15,(hiddens,hiddens))
+		dco1=np.zeros(shape=(hiddens,1))
 		_i=0
 		_bloss=0
 		_loss=0
-		batch_mem=100
-		trainingpbatch=0
+		batch_mem=20
+		trainingpbatch=1
 		it=0
 		c=0
 		while it<len(total_sen) and _loop:
@@ -67,13 +71,11 @@ def WordtoVec():
 				else:
 					it=0
 					_i=0
-					trainingpbatch=0
+					trainingpbatch=1
 					print(it," loss ",_bloss/batch_mem)
 					c+=1
 					_graph.write(str(c)+" "+str(_bloss/batch_mem)+"\n")
 				_bloss=0
-			if len(words)<3:
-				continue
 			states=np.zeros(shape=(len(words)-1,1,hiddens))
 			res=np.array(int(words[len(words)-1])).reshape(1,1)
 			for w in range(0,len(words)-1):
@@ -82,50 +84,48 @@ def WordtoVec():
 				x_=wordvec[k].reshape(worddimx,1,worddimy)
 				if w<len(words)-2:
 					if w==0:
-						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co}
+						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,coef2:co2}
 					else:
-						inp={actx:x_,pre:states[w-1],coef:co}
+						inp={actx:x_,pre:states[w-1],coef:co,coef2:co2}
 					sess.run([assignx],feed_dict=inp)
 					states[w]=sess.run(state,feed_dict=inp)
 				else:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res}
-					z=sess.run([state,loss],feed_dict=inp)
-					#print(z[0][0],res)
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
+					z=sess.run([state,loss,final],feed_dict=inp)
+					#print(z[2][0],res)
+					#print(z[0])
 					states[w]=z[0]
 					_loss=z[1][0][0]
 			_bloss+=_loss
 			if(_loss<0.00000001):
 				continue
 			dpres=[[0]]
-			_dcoef=np.zeros(shape=(worddimx,worddimy,hiddens))
 			for w in reversed(range(0,len(words)-1)):
 				k=int(words[w])
 				x_=wordvec[k].reshape(worddimx,1,worddimy)
 				if w==len(words)-2:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res}
-					z=sess.run([dxlast,dstatelast,dcoeflast],feed_dict=inp)
-					wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*abs(np.random.normal(0,.1*_loss))
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
+					z=sess.run([dxlast,dstatelast,dcoef1],feed_dict=inp)
+					wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.15
+					#co1-=z[2][0]*.1
 					#print(z[0][0].reshape(worddimx,worddimy)*.5)
-					#_dcoef+=z[2][0]
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:z[1][0]}
+					inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:z[1][0],coef2:co2,coef1:co1}
 					dpres=(sess.run(dpre,feed_dict=inp))[0]
 				else:
 					if w>0:
-						inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:dpres}
-						z=sess.run([dpre,dx,dcoef],feed_dict=inp)
+						inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:dpres,coef2:co2}
+						z=sess.run([dpre,dx],feed_dict=inp)
 						dpres=z[0][0]
-						#_dcoef+=z[2][0]
-						wordvec[k]-=z[1][0].reshape(worddimx,worddimy)*abs(np.random.normal(0,.1*_loss))
+						wordvec[k]-=z[1][0].reshape(worddimx,worddimy)*.15
 						#print(z[1][0].reshape(worddimx,worddimy)*.4)
 					else:
-						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,actual:res,pregrad:dpres}
-						z=sess.run([dx,dcoef],feed_dict=inp)
-						wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*abs(np.random.normal(0,.1*_loss))
+						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,actual:res,pregrad:dpres,coef2:co2}
+						z=sess.run([dx],feed_dict=inp)
+						wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.15
 						#print(z[0][0].reshape(worddimx,worddimy)*.4)
-						#_dcoef+=z[1][0]
 				#co-=_dcoef*.1
 	_graph.close()
 signal.signal(signal.SIGINT, sig)
-#dictionary,good_sentence,bad_sentence,wordvec,total_sen=data1.PrepareData(worddimx,worddimy)
-dictionary,total_sen,wordvec=data1.GetData(worddimx,worddimy)
+#good_sentence,bad_sentence,wordvec,total_sen=data1.PrepareData(worddimx,worddimy)
+total_sen,wordvec=data1.GetData(worddimx,worddimy)
 WordtoVec()
