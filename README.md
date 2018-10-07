165551552dba5636605b14b39a84937e99a98c5d
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2018-10-07 22:46:41 +0530

    update

--- README.md
+++
@@ -1,8 +0,0 @@
-# PredictNextWord
-Learning to predict the next word in a sentence (English)   
-Just started this. Here we try to build a model which can predict the next probable word in a english sentence.   
-Data:-   
-https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html      
-I am also working another set of n-gram data which I have collected from https://www.ngrams.info/
-#### TODO   
-1. Build a WordtoVec model.   
--- test.py
+++ test.py
@@ -1,131 +1,100 @@
-import tensorflow as tf
 import numpy as np
-import data
-import data1
-import signal
-good_sentence=list()
-bad_sentence=list()
-total_sen=list()
-wordvec=0
-worddimy=200
-worddimx=1
-hiddens=50
-_loop=1
-def sig(signal,frame):
-	global _loop
-	_loop=0
-def body(rawstate,x,v,coef):
-	_r=rawstate+tf.matmul(x[v],coef[v])
-	_v=v+1
-	return [_r,x,_v,coef]
-def cond(rawstate,x,v,coef):
-	return v<worddimx
-def WordtoVec():
-	dt=tf.float32
-	pre=tf.placeholder(name="pre",dtype=dt,shape=(1,hiddens))
-	coef=tf.placeholder(name="coef",dtype=dt,shape=(worddimx,worddimy,hiddens))
-	coef1=tf.placeholder(name="coef",dtype=dt,shape=(hiddens,1))
-	coef2=tf.placeholder(name="coef",dtype=dt,shape=(hiddens,hiddens))
-	actual=tf.placeholder(name="actual",dtype=dt,shape=(1,1))
-	pregrad=tf.placeholder(name="stategradactual",dtype=dt,shape=(1,hiddens))
-	x=tf.get_variable("x1",shape=[worddimx,1,worddimy])
-	actx=tf.placeholder(name="actx",dtype=dt,shape=(worddimx,1,worddimy))
-	assignx=tf.assign(x,actx)
-	rawstate=tf.zeros(shape=(1,hiddens),dtype=dt,name="rawstate")
-	v=tf.constant(0)
-	calrawstate=tf.while_loop(cond,body,[rawstate,x,v,coef])[0]
-	state=(calrawstate+tf.matmul(pre,coef2))
-	final=tf.nn.softsign(tf.matmul(state,coef1))
-	loss=tf.square(final-actual)
-	dxlast=tf.gradients(loss,x,name="lastxgrad")
-	dstatelast=tf.gradients(loss,state,name="laststategrad")
-	dcoeflast=tf.gradients(loss,coef,name="laststategrad")
-	dpre=tf.gradients(state,pre,grad_ys=pregrad,name="pregrad")
-	dx=tf.gradients(state,x,grad_ys=pregrad,name="xgrad")
-	dcoef1=tf.gradients(loss,coef1,name="dcoef1")
-	_graph=open("~graph","w")
-	with tf.Session() as sess:
-		writer = tf.summary.FileWriter("tfg", sess.graph)
-		co=np.random.uniform(-.5,.5,(worddimx,worddimy,hiddens))
-		co1=np.random.uniform(-.2,.2,(hiddens,1))
-		co2=np.random.uniform(-.15,.15,(hiddens,hiddens))
-		dco1=np.zeros(shape=(hiddens,1))
-		_i=0
-		_bloss=0
-		_loss=0
-		batch_mem=20
-		trainingpbatch=1
-		it=0
-		c=0
-		while it<len(total_sen) and _loop:
-			#words=total_sen[2526]
-			#print("Completed: ",_i/len(total_sen)*100)
-			words=total_sen[it]
-			it+=1
-			_i+=1
-			if _i==batch_mem:
-				if trainingpbatch>0:
-					it-=_i
-					_i=0
-					trainingpbatch-=1
-				else:
-					it=0
-					_i=0
-					trainingpbatch=1
-					print(it," loss ",_bloss/batch_mem)
-					c+=1
-					_graph.write(str(c)+" "+str(_bloss/batch_mem)+"\n")
-				_bloss=0
-			states=np.zeros(shape=(len(words)-1,1,hiddens))
-			res=np.array(int(words[len(words)-1])).reshape(1,1)
-			for w in range(0,len(words)-1):
-				k=int(words[w])
-				#print(dictionary[int(words[w])])
-				x_=wordvec[k].reshape(worddimx,1,worddimy)
-				if w<len(words)-2:
-					if w==0:
-						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,coef2:co2}
-					else:
-						inp={actx:x_,pre:states[w-1],coef:co,coef2:co2}
-					sess.run([assignx],feed_dict=inp)
-					states[w]=sess.run(state,feed_dict=inp)
-				else:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
-					z=sess.run([state,loss,final],feed_dict=inp)
-					#print(z[2][0],res)
-					#print(z[0])
-					states[w]=z[0]
-					_loss=z[1][0][0]
-			_bloss+=_loss
-			if(_loss<0.00000001):
-				continue
-			dpres=[[0]]
-			for w in reversed(range(0,len(words)-1)):
-				k=int(words[w])
-				x_=wordvec[k].reshape(worddimx,1,worddimy)
-				if w==len(words)-2:
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,coef2:co2,coef1:co1}
-					z=sess.run([dxlast,dstatelast,dcoef1],feed_dict=inp)
-					wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.15
-					#co1-=z[2][0]*.1
-					#print(z[0][0].reshape(worddimx,worddimy)*.5)
-					inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:z[1][0],coef2:co2,coef1:co1}
-					dpres=(sess.run(dpre,feed_dict=inp))[0]
-				else:
-					if w>0:
-						inp={actx:x_,pre:states[w-1],coef:co,actual:res,pregrad:dpres,coef2:co2}
-						z=sess.run([dpre,dx],feed_dict=inp)
-						dpres=z[0][0]
-						wordvec[k]-=z[1][0].reshape(worddimx,worddimy)*.15
-						#print(z[1][0].reshape(worddimx,worddimy)*.4)
-					else:
-						inp={actx:x_,pre:np.zeros(shape=(1,hiddens)),coef:co,actual:res,pregrad:dpres,coef2:co2}
-						z=sess.run([dx],feed_dict=inp)
-						wordvec[k]-=z[0][0].reshape(worddimx,worddimy)*.15
-						#print(z[0][0].reshape(worddimx,worddimy)*.4)
-				#co-=_dcoef*.1
-	_graph.close()
-signal.signal(signal.SIGINT, sig)
-#good_sentence,bad_sentence,wordvec,total_sen=data1.PrepareData(worddimx,worddimy)
-total_sen,wordvec=data1.GetData(worddimx,worddimy)
-WordtoVec()
+import matplotlib.pyplot as plt
+from tsne import tsne
+
+
+dim = 150
+lines = []
+word_index = {}
+word_occ = {}
+count = 0
+
+with open('~w5_.txt') as data_file:
+    for line in data_file:
+        if np.random.random() > 0.2:
+            continue
+        line = line.split('\t')
+        line.pop(0)
+        line = [l.strip().lower() for l in line]
+        for word in line:
+            if word not in word_index:
+                word_index[word] = count
+                count += 1
+                word_occ[word] = 1
+            else:
+                word_occ[word] += 1
+        lines.append(line)
+
+print(len(lines))
+print(len(word_index))
+embedding_matrix = np.random.normal(size=(len(word_index), dim), scale=0.01)
+pmf = np.ones((len(word_index),))
+for word, num in word_index.items():
+    pmf[num] = word_occ[word] ** 0.75
+pmf /= np.sum(pmf)
+print(np.sum(pmf))
+
+
+def apply_derivative(a_index, b_index, choices):
+    global embedding_matrix
+    choices = np.setdiff1d(choices, np.array([a_index, b_index]))
+    temp = np.exp(np.matmul(embedding_matrix[choices, dim // 2:],
+                            embedding_matrix[a_index][:dim // 2]))
+    temp_with_b = np.append(temp, np.exp(np.dot(
+        embedding_matrix[a_index][:dim // 2], embedding_matrix[b_index][dim // 2:])))
+    sum_temp = np.sum(temp_with_b)
+    delta_embedding_matrix = np.zeros((len(choices) + 2, dim // 2))
+    delta_embedding_matrix[-1] = embedding_matrix[b_index][dim // 2:] - \
+        (np.matmul(embedding_matrix[choices, dim // 2:].T, temp)
+         + embedding_matrix[b_index][dim // 2:] * temp_with_b[-1]) / sum_temp
+    delta_embedding_matrix[-2] = embedding_matrix[a_index][:dim // 2] - \
+        embedding_matrix[a_index][:dim // 2] * temp_with_b[-1] / sum_temp
+
+    delta_embedding_matrix[:-2, :] = -np.tile(np.expand_dims(embedding_matrix[a_index][:dim // 2], 0), (len(
+        choices), 1)) * np.tile(np.expand_dims(temp, 1), (1, dim // 2)) / sum_temp
+
+    embedding_matrix[choices, dim // 2:] += 0.05 * \
+        delta_embedding_matrix[:-2, :]
+    embedding_matrix[b_index][dim // 2:] += 0.05 * delta_embedding_matrix[-2]
+    embedding_matrix[a_index][:dim // 2] += 0.05 * delta_embedding_matrix[-1]
+
+
+# choices = np.arange(0, len(word_index), 1)
+# for i in range(200):
+#     apply_derivative(0, 1, choices)
+#     print(i, np.exp(np.dot(embedding_matrix[0][:dim // 2], embedding_matrix[1][dim // 2:])) / np.sum(
+#         np.exp(np.matmul(embedding_matrix[:, dim // 2:], embedding_matrix[0][:dim // 2]))))
+
+
+for i in range(10):
+    np.random.shuffle(lines)
+    for line in lines:
+        choices = np.random.choice(len(embedding_matrix), 10, False, pmf)
+        for j in range(0, 4):
+            apply_derivative(word_index[line[j]],
+                             word_index[line[j + 1]], choices)
+    print(i)
+
+inv_map = {v: k for k, v in word_index.items()}
+
+for i in range(300):
+    a = np.random.randint(0, len(word_index))
+    print(inv_map[a], end=' - ')
+    b = np.exp(np.matmul(embedding_matrix[:, dim // 2:], embedding_matrix[a][:dim // 2]))
+    b = b / np.sum(b)
+    b[a] = -1
+    temp = sorted(np.arange(0, len(word_index), 1),
+                  key=lambda x: b[x], reverse=True)[:10]
+    for w in temp:
+        print(inv_map[w], '(', b[w], ')', end=',   ')
+    print(end='\n\n')
+print()
+
+# reduced_embedding_matrix = tsne(embedding_matrix/100, 2, 50, 20)
+# fig, ax = plt.subplots()
+# for txt in word_index.keys():
+#     ax.annotate(
+#         txt, (reduced_embedding_matrix[word_index[txt]][0], reduced_embedding_matrix[word_index[txt]][1]))
+# ax.set_ylim(-100, 100)
+# ax.set_xlim(-100, 100)
+# plt.show()
